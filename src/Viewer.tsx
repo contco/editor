0d029a446e5f7249400946988b81b5d8ba8e616e
Hamza Alam <smhamza124@gmail.com>
2020-12-04 02:33:35 +0500

    fix

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -77,6 +77,6 @@ const Viewer: (props: ViewerProps) => any = ({ data }) => {
     });
     return render;
   };
-  return <>{renderBlock()}</>;
+  return <div>{renderBlock()}</div>;
 };
 export default Viewer;
