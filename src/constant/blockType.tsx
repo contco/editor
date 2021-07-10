78cba0cd096ac0ae52b5d1514154a43c94eb22ef
Hamza Alam <getright_02@live.com>
2021-07-10 22:03:02 +0500

    list item

--- src/constant/blockType.tsx
+++ src/constant/blockType.tsx
@@ -1,4 +1,6 @@
 export const HEADING1 = 'heading-one';
 export const HEADING2 = 'heading-two';
 export const BLOCK_QUOTE = 'block-quote';
+export const NUMBERED_LIST = 'numbered-list';
+export const BULLETED_LIST = 'bulleted-list';
 export const TEXT = 'text';
