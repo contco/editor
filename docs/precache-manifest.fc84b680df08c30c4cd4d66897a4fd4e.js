0738da9db1199960ec1abddab7abd3859ab0a35c
Hamza Alam <getright_02@live.com>
2023-06-11 13:54:22 +0200

    gh-pages

---
+++ docs/precache-manifest.fc84b680df08c30c4cd4d66897a4fd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d790546e0a3b3c2753644425287786",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a6d0cd5a3b21a42e9ce",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "8d16c059f5919e584390",
+    "url": "./static/js/2.4538d19c.chunk.js"
+  },
+  {
+    "revision": "f59aad082934a74ed9c144d141725f01",
+    "url": "./static/js/2.4538d19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6d0cd5a3b21a42e9ce",
+    "url": "./static/js/main.8bbdbb77.chunk.js"
+  },
+  {
+    "revision": "2549de9f135ed36fdc1c",
+    "url": "./static/js/runtime-main.8429d527.js"
+  }
+]);
